489488542572e44ef203fca7e34d63e816100afa
Majangajohn <47883618+Majangajohn@users.noreply.github.com>
2024-06-28 11:21:44 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,17 @@
-/*
- * FILE: 0-isupper.c 
- * Auth: Nyange
- */
-#include <ctype.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
  * _isupper - uppercase letters
- * @c: character to chec
+ * @c: character to check
  *
  * Return: 0 or 1
  */
 int _isupper(int c)
+
 {
-	if (isupper(c))
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
+
 }
