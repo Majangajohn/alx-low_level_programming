5be9b9c7a4dc010ed45b4603727a3462efc0863d
Majangajohn <47883618+Majangajohn@users.noreply.github.com>
2024-06-28 11:01:32 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,17 +9,12 @@
  * _isupper - function that checks for uppercase character
  * Returns 1 when upper and 0 when lower
  *
- * Return: 1 when is upper, 0 when lower
+ * Return: 0 or 1
  */
-
 int _isupper(int c)
 {
 	if (isupper(c))
-		
 		return (1);
 	else
-
 		return (0);
-
-
 }
