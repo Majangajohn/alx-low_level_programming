2940f6f315f72afc5d9860023144e9d2d03cc038
@Iam_Nyange <jnmajang@gmail.com>
2024-07-01 13:22:42 -0700

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+/*
+ * FILE: 1-swap.c
+ * @Auth: Nyange
+ *
+ */
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first value
+ * @b: pointer to second value
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-strlen.c
+ * @Auth: Nyange
+ *
+ */
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = strlen(*s);	
+
+	return (i);
+}
