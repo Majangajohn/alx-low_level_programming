dc1caa7a744e26751850c6a629fbf25d92433edb
Majangajohn <47883618+Majangajohn@users.noreply.github.com>
2024-07-04 08:59:18 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,27 +3,18 @@
  * @Auth: Nyange
  *
  */
+
 #include "main.h"
 #include <string.h>
+
 /**
  * _strcat - concatenate strings
  * @dest: string 1
  * @src: string 2
+ *
  * Return: NULL if failure
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, c;
-
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-
-	for (c = 0; src[c] != '\0'; c++)
-	{
-		dest[i] = src[c];
-		i++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
+	return (strcat(dest, src));
 }
