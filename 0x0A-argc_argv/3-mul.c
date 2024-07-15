3b5280f833770f1f60abbf931617a2edb2752051
@Iam_Nyange <jnmajang@gmail.com>
2024-07-15 04:24:08 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,29 @@
+/*
+ * FILE: 3-mul.c
+ * @AUth: Nyange
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - main code execution 
+ * main - code execution takes place
  * @argc: The size of the argv array.
  * @argv: An array containing the program command line arguments
  *
  * Return: Always 0 || 1.
  */
+
 int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
 		printf("%s\n", "Error");
+
 		return (1);
 	}
+
 	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+
 	return (0);
 }
