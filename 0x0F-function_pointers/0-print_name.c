b2839ee4ea285597935d79ddbccdce47d031fe5e
@Iam_Nyange <jnmajang@gmail.com>
2024-07-24 01:29:55 -0700

    a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+/*
+ * FILE: 0-print_name.c
+ * @Auth: Nyange
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name from a function pointer
+ * @name: char string
+ * @f: function pointer that takes a string argument
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
