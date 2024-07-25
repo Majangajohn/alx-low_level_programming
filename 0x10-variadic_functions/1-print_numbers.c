0324c4b53c5ec844a8a0f7f4eb5d1dd9622283fc
@Iam_Nyange <jnmajang@gmail.com>
2024-07-25 05:16:39 -0700

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,15 +22,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 
 	if (n > 0)
-
 		printf("%d", va_arg(list, int));
-
 	for (i = 1; i < n; i++)
-
 		printf("%s%d", sep, va_arg(list, int));
-
 	printf("\n");
-	
-
 	va_end(list);
 }
